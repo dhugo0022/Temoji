03d569d41dbb8e41bc183f51049585b0c690117f
dhugo0022 <alirocha20111@gmail.com>
2023-06-09 19:13:19 -0300

    Main component update

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,19 @@
-import './style.css'
-import ReactDOM from "react-dom/client";
-import App from "./components/App";
+import { createRoot } from "react-dom/client";
+import React from 'react';
+import { Provider } from "react-redux";
+import ReactModal from "react-modal";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <App />
+import "./style.css";
+import App from "./windows/App/App";
+import { store } from "./store";
+
+// Modal
+ReactModal.setAppElement("#root");
+
+createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
